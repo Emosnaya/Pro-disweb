2c6c09f33a40c66e9d75f204e72c507d083f32db
Emosnaya <emmanuel.osnaya@ingenieria.unam.edu>
2022-04-01 01:40:58 -0600

    Se hizo la conexion a la base de datos

--- PHP/config.php
+++ PHP/config.php
@@ -1,11 +1,6 @@
 <?php
-    //Código base para usar base de datos
-    define("DBUSER","root");
-    define("DBHOST","localhost");
-    define("PASSWORD","");
-    define("DB","desarrolloweb");
     //funcion para conectar la base de datos 
-    function connectdb (){
+    function connectdb() {
         $con=mysqli_connect('localhost', 'root','', 'desarrolloweb');
         if(!$con){
             echo "no se pudo acceder a la base de datos";
